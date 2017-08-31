5491c9c416f62ca3954121e2291237891adb50d8
Emmy Emme <masteremmy.js@gmail.com>
2017-08-31 11:57:41 -0400

    Dependencies, notes

--- bootstrap_bash.sh
+++ bootstrap_bash.sh
@@ -9,10 +9,13 @@ sudo apt-get -y upgrade
 # ssh key
 
 # python / git
-sudo apt-get -y install python-setuptools python-dev git 
+sudo apt-get -y install python-setuptools python-dev git
 sudo easy_install pip
 
 # virtualenv
 sudo pip install virtualenvwrapper wiringpi wiringpi2
 mkdir $HOME/Devel
-mkdir $HOME/.virtualenvs 
+mkdir $HOME/.virtualenvs
+
+# want to install phantom.js?
+# sudo ./phantomjs.sh
---
+++ mod.py
@@ -0,0 +1,4 @@
+import time, json, curses, wiringpi
+
+
+#import a class and a method by which to calibrate pulse
