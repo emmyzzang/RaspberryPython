723d33d23f4b96d3ba3c9671ee835050efee02ac
Emmy Emme <masteremmy.js@gmail.com>
2017-09-01 22:24:27 -0400

    Class calibrator

--- mod.py
+++ mod.py
@@ -1,4 +1,11 @@
 import time, json, curses, wiringpi
 
 
-#import a class and a method by which to calibrate pulse
+# import a class and a method by which to calibrate pulse
+# https://stackoverflow.com/questions/6013844/python-init-syntax 
+class Calibrator(object); 
+	def __init__ (self, [arg?], pin=PIN1, calibration_file=None); 
+		# super(Child, self)
+		super (Calibrator, self).__init__()
+			self.pin = pin
+			#hadouken syntax 
